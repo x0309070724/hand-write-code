7fd1b181d24ec918212f197dd5f9a5c01edce660
肖道光 <12068742@qq.com>
2022-06-08 14:50:47 +0800

    feat: add handle write

---
+++ src/write-code/instanceOf.js
@@ -0,0 +1,12 @@
+const instanceOf = (A, B) => {
+  let p = A;
+  while (p) {
+    if (p === B.prototype) {
+      return true;
+    }
+    p = p.__proto__;
+  }
+  return false;
+};
+
+instanceOf([],Array)
